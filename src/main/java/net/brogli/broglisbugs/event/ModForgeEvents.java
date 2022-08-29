0b90c8e87210d5b11872a71c642293f022ad1093
AreYouW <44926928+AreYouW@users.noreply.github.com>
2022-08-29 20:18:54 +0100

    Update Stick insect to use the leaves tag

--- src/main/java/net/brogli/broglisbugs/event/ModForgeEvents.java
+++ src/main/java/net/brogli/broglisbugs/event/ModForgeEvents.java
@@ -9,7 +9,6 @@ import net.brogli.broglisbugs.entity.custom.EntityWorm;
 import net.brogli.broglisbugs.util.ModTagsUtils;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -18,27 +17,28 @@ import net.minecraftforge.fml.common.Mod;
 public class ModForgeEvents {
 
     @SubscribeEvent
-    public static void HarvestDropsEvent(final BlockEvent.BreakEvent event ) {
+    public static void HarvestDropsEvent(final BlockEvent.BreakEvent event) {
         Random random = new Random();
         float RandF = random.nextFloat();
-        if (event.getLevel() instanceof Level){
-            if (event.getState().getBlock() instanceof LeavesBlock){
+        if (event.getLevel() instanceof Level) {
+            if (ModTagsUtils.isMemberOfBlockTag(BlockTags.LEAVES, event.getState().getBlock())) {
                 if (RandF >= 0.0F && RandF <= 0.05F) {
-                    System.out.println("Stick Insect Appeared");
-                    EntityStickInsect entityStickInsect = new EntityStickInsect(BroglisBugsEntityTypes.ENTITY_STICK_INSECT.get(), (Level) event.getLevel());
+                    EntityStickInsect entityStickInsect = new EntityStickInsect(
+                            BroglisBugsEntityTypes.ENTITY_STICK_INSECT.get(), (Level) event.getLevel());
                     entityStickInsect.setPos(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ());
                     event.getLevel().addFreshEntity(entityStickInsect);
                 }
             }
 
-            if (ModTagsUtils.isMemberOfBlockTag(BlockTags.DIRT, event.getState().getBlock())){
+            if (ModTagsUtils.isMemberOfBlockTag(BlockTags.DIRT, event.getState().getBlock())) {
                 if (RandF >= 0.0F && RandF <= 0.02F) {
-                    EntityWorm entityWorm = new EntityWorm(BroglisBugsEntityTypes.ENTITY_WORM.get(), (Level) event.getLevel());
-                    entityWorm.setPos(event.getPos().getX() + 0.5F, event.getPos().getY(), event.getPos().getZ() + 0.5F);
+                    EntityWorm entityWorm = new EntityWorm(
+                        BroglisBugsEntityTypes.ENTITY_WORM.get(), (Level) event.getLevel());
+                    entityWorm.setPos(event.getPos().getX() + 0.5F, event.getPos().getY(),
+                            event.getPos().getZ() + 0.5F);
                     event.getLevel().addFreshEntity(entityWorm);
                 }
             }
         }
     }
 }
-
